5d97c598025cb4b886485c8b2ec084f40e2edf26
tuns <nguyensinhtuit97@gmail.com>
2018-11-08 00:16:27 +0700

    second commit

--- MR/src/com/mr/WordCount.java
+++ MR/src/com/mr/WordCount.java
@@ -11,14 +11,11 @@ package com.mr;
  */
 
 import java.io.IOException;
-import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 
 
@@ -35,30 +32,72 @@ public class WordCount {
     
     // chường trìng : mapper ==> combiner ==> reducer
     // map phrase
-    // input : text và số lượng text
-     public class Map extends Mapper<Text, IntWritable, Text, IntWritable>
+    // input của Map phrase <byteoffset(kiểu long), text>
+    // ouput : <key : text, value : 1>
+     public class Map extends Mapper<LongWritable, Text, Text, IntWritable>
     {
         // hadoop cung cấp writable interface để serialize và 
         // de-serialization data được lưu trữ trong HDFS, và input và ouput giữa các mapreduce
         private Text word = new Text();
         private IntWritable one = new IntWritable(1);
         
-        // LongWritable thực chất là keiu623 long là vi trí của position
-        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
-                throws IOException{
+        // LongWritable thực chất là keiu623 long là vi trí của dòng trong file
+        // mapper context object nhận configuation, job, từ contructor của nó dùng để communicate giữa các map phrase
+        // và giữa map phrase và reduce phrase
+        public void map(LongWritable key, Text value, Context output, Reporter reporter)
+                throws IOException, InterruptedException{
             String line = value.toString();
             
             String []str = line.split(" ");
             
             for (int i = 0; i < str.length; ++i){
                 word.set(str[i]);
-                output.collect(word, one);
+                // write intermedia data between map phrase
+                output.write(word, one);
             }
         }
     }
      
+     // combiner phrase : lấy ouput của map phrase xử lý, ouput là key-value collection
      // reduce phrase
-     public class Reduce extends Reducer<Object, Object, Object, Object>{
+     // intput : key, values collection
+     public class Reduce extends Reducer<Text, IntWritable, Text, IntWritable>{
          
+         // context 
+         public void reduce (Text key, Iterable<IntWritable> collect, Context context) throws IOException, InterruptedException{
+             int sum = 0; // số lần của key xuất hiện trong file
+             for (IntWritable v : collect){
+                 sum += v.get();
+             }
+             
+             IntWritable result = new IntWritable(sum);
+             
+             context.write(key, result);
+         }
+     }
+     
+     
+     public static void main(String args[])throws Exception{
+         // job configuation
+         
+         Configuration conf = new Configuration();
+         Job job = Job.getInstance(conf, "word count");
+         // set class mà hadoop sẽ xác định file .jar nào được send tới các nodes để thực hiện
+         // map task và reduce task (mapp class, reduce task phải là một phần của class này)
+         job.setJarByClass(WordCount.class);
+         
+         job.setMapperClass(Map.class);
+         job.setReducerClass(Reduce.class);
+         job.setCombinerClass(Reduce.class);
+         
+         job.setInputFormatClass(TextInputFormat.class);
+         
+         job.setOutputKeyClass(Text.class);
+         job.setOutputValueClass(IntWritable.class);
+         
+         FileInputFormat.addInputPath(job, new Path(args[1]));
+         FileOutputFormat.setOutputPath(job, new Path(args[2]));
+         
+         System.exit(job.waitForCompletion(true) ? 0 : 1);
      }
 }
